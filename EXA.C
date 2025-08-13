f096a2067f61a28825ef65689e4f66a14e314206
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2025-08-13 09:55:53 -0300

    Update EXA.C

--- EXA.C
+++ EXA.C
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-main()
+int main(void)
 { int c;
   
 
-  for( c=9220; c<9400; c++)
+  for( c=0; c<128; c++)
   {
    printf("%d\t", c);
    printf("%x\t", c);
+   printf("\n");
   }
    return 0;
 }
