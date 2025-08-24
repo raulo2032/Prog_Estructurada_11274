71a9d2d32ad37b77aca46ebd74088a1ab15e5217
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2025-08-24 17:21:41 -0300

    Update carga_vector_1.c

--- carga_vector_1.c
+++ carga_vector_1.c
@@ -11,7 +11,7 @@ int main(void) {
 
     printf("\n El valor de los elementos del vector se carga aleatoriamente ");
     for ( i = 0; i < TAMANIO; i++){
-        vector[i] = rand()%100; /* aleatorio entre 0 y 99*/ 
+        vector[i] = rand()%100; /* aleatorio entre 0 y 99*/  //        vector[i]= rand() % 10;  carga con elementos entre 0 y 10
         printf("enteros[%d] = %d \n", i, vector[i]);
         
     }
