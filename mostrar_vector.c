2fecb9f2b0c47017db21ae57f1a9c77743eb953c
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2025-08-24 15:43:23 -0300

    se carga aleatoriamente

--- mostrar_vector.c
+++ mostrar_vector.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define TAMANIO 5
 
 void mostrar_vector(int [], int);
+void cargar_vector(int []);
+
 int main(void){
-    int vector[TAMANIO]={5,6,9,2,8};
+
+    int vector[TAMANIO];
+    cargar_vector(vector);
     mostrar_vector(vector, TAMANIO);
 }
 
+
+void cargar_vector(int vector[]){
+    int i=0;
+    srand(time(NULL));
+    for(i=0; i<TAMANIO; i++){
+        vector[i]= rand() % 10;
+    }
+    
+}
 void mostrar_vector(int vector[], int){
     int i;
 
