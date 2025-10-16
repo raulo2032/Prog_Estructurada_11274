7cb27b13a002a2a1eb94258615057a176a28793f
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2025-10-16 14:47:29 -0300

    Create tp6_eje05.c

---
+++ tp6_eje05.c
@@ -0,0 +1,5 @@
+Utilizando el registro del punto anterior, crear las siguientes funciones:
+Una función que permita la carga de un vector de 20 músicos.
+Una función que reciba el vector de músicos y retorne el nombre del músico con mayor cantidad de discos.
+Una función que reciba el vector de músicos, una edad y retorne el promedio de discos para los músicos menores a la edad recibida.
+Una función que reciba el vector de músicos, un año e imprima por pantalla el nombre de los discos cuyo año de lanzamiento sea posterior al recibido cómo parámetro. Además, se debe incluir el nombre del artista, el género y la cantidad de canciones del disco.
